9cb543711becbb094c905d4c4e257d4c5acdf89e
Marco Espinoza <marco_a_espinoza@yahoo.com>
2019-10-26 00:15:53 -0700

    add sound effects and sound manager

---
+++ Match App/SoundManager.swift
@@ -0,0 +1,62 @@
+//
+//  SoundManager.swift
+//  Match App
+//
+//  Created by Marco Espinoza on 10/25/19.
+//  Copyright © 2019 Marco Espinoza. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class SoundManger {
+
+    var audioPlayer: AVAudioPlayer?
+    var soundFilename: String?
+    
+    enum SoundEffect {
+        case flip
+        case match
+        case nomatch
+        case shuffle
+    }
+    
+    func playSound(_ effect: SoundEffect) {
+        
+        // Determine cases for sound effects
+        switch effect {
+            case .flip:
+                soundFilename = "cardflip"
+            case .match:
+                soundFilename = "dingcorrect"
+            case .nomatch:
+                soundFilename = "dingwrong"
+            case .shuffle:
+                soundFilename = "shuffle"
+        }
+        
+        // Get path to the sound file in bundle
+        let bundlePath = Bundle.main.path(forResource: soundFilename, ofType: "wav")
+        guard bundlePath != nil else {
+            print("Not able to find filename: \(String(describing: soundFilename)) in the bundle.")
+            return
+        }
+        
+        // Create URL object from string path
+        let soundUrl = URL(fileURLWithPath: bundlePath!)
+        
+        do {
+            
+            // Create audio player object
+            audioPlayer = try AVAudioPlayer(contentsOf: soundUrl)
+            audioPlayer?.play()
+        }
+        catch {
+            
+            // Error handling: couldn't create audio player object
+            print("Coudn't create the audio player object sound file \(String(describing: soundFilename))")
+        }
+    }
+    
+}
+
