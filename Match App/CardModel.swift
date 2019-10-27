e59aae009b5dc9dd4ac77ad094b53a39c082a9d9
Marco Espinoza <marco_a_espinoza@yahoo.com>
2019-10-27 00:09:29 -0700

    create unique pairs and randomize shuffle

--- Match App/CardModel.swift
+++ Match App/CardModel.swift
@@ -4,39 +4,48 @@ class CardModel {
     
     func getCards() -> [Card] {
         
+        // Declare an array to store numbers already generated
+        var generatedNumbersArray = [Int]()
+        
         // Declare an array to store generated cards
         var generatedArrayOfCards = [Card]()
         
         // Randomly generate pairs of cards
-        for _ in 1...8 {
+        while generatedNumbersArray.count < 8 {
             
             // Create random number
-            let randomNumber = String(arc4random_uniform(13) + 1)
+            let randomNumber = arc4random_uniform(13) + 1
             
-            // Log the number
-            print(randomNumber)
+            // Ensure random number isn't the same in array
+            if generatedNumbersArray.contains(Int(randomNumber)) == false {
+                
+                // Log the number
+                print(randomNumber)
+                
+                // Store number into generatedNumbersArray
+                generatedNumbersArray.append(Int(randomNumber))
+                
+                // Create first card object
+                let cardOne = Card()
+                cardOne.imageName = "card\(randomNumber)"
+                
+                // Add created card in array
+                generatedArrayOfCards.append(cardOne)
+                
+                // Create Second card object
+                let cardTwo = Card()
+                cardTwo.imageName = "card\(randomNumber)"
+                
+                // Add second card in array
+                generatedArrayOfCards.append(cardTwo)
+            }
             
-            // Create first card object
-            let cardOne = Card()
-            cardOne.imageName = "card\(randomNumber)"
-            
-            // Add created card in array
-            generatedArrayOfCards.append(cardOne)
-            
-            // Create Second card object
-            let cardTwo = Card()
-            cardTwo.imageName = "card\(randomNumber)"
-            
-            // Add second card in array
-            generatedArrayOfCards.append(cardTwo)
-            
-            // TODO: Make it so we only have unique pairs of cards
+            // Make it so we only have unique pairs of cards
             
         }
-        // TODO: Randomize the array
         
-        // Return the array
-        return generatedArrayOfCards
+        // Return the array of cards shuffled
+        return generatedArrayOfCards.shuffled()
     }
     
 }
